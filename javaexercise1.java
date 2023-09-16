557a828f8e0cf85b8c3dfbcc4f13abc48db3d871
Rohan Chauhan <140747527+Rohan-rccodes@users.noreply.github.com>
2023-09-16 13:26:16 +0530

    first test commit massage

---
+++ javaexercise1.java
@@ -0,0 +1,15 @@
+
+class exercise1 
+{
+	public static void main(String[] args)
+
+	{
+
+	System.out.print("Hello! \n My name is Rohan");
+
+	System.out.print("Thgis is test liine for java output");
+
+
+	}
+}
+	
---
+++ javaexercise2.java
@@ -0,0 +1,11 @@
+class exercise2
+{
+public static void main(String[] args)
+
+{
+    System.out.println("ADDING TWO NMUMBERS : ");
+
+    System.out.print(10+20);
+
+}
+}
